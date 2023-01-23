b8f289ea8a85e8c3e0867a889bb59750de3f3f31
Stefan Eng <stefaneng@mednet.ucla.edu>
2023-01-23 12:58:45 -0800

    Init commit with pkdown

---
+++ R/hello.R
@@ -0,0 +1,12 @@
+#' Hello
+#'
+#' @param x example
+#'
+#' @return
+#' @export
+#' 
+#' @examples
+#' BoutrosLab.plotting.general::create.histogram(x = rnorm(1000))
+hello <- function(x = 1) {
+    print(x)
+}
